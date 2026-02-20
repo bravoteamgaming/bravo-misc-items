022db86b790c640630cc32ea55633d94931be210
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-02-20 22:30:54 +0000

    Add 7.62 NATO

--- bravo_rhs_ammo/cfgAmmo.hpp
+++ bravo_rhs_ammo/cfgAmmo.hpp
@@ -1,5 +1,6 @@
 class CfgAmmo
 {
+	class BulletBase;
 	class B_762x51_Ball;
 	class B_556x45_Ball;
 	
@@ -19,5 +20,31 @@ class CfgAmmo
 	class rhs_B_762x54_Ball : B_762x51_Ball
 	{
 		caliber = 0.53;
+	};
+	
+	class rhs_ammo_762x51_M80_Ball : BulletBase
+	{
+		caliber = 1.1;
+	};
+	class rhs_ammo_762x51_M118_Special_Ball : rhs_ammo_762x51_M80_Ball
+	{
+		caliber = 1.8;
+		hit = 16;
+	};
+	class rhs_ammo_762x51_M993_Ball : rhs_ammo_762x51_M80_Ball
+	{
+		caliber = 2.2;
+	};
+	class rhs_ammo_762x51_M80A1EPR_Ball : rhs_ammo_762x51_M80_Ball
+	{
+		caliber = 1.2;
+	};
+	class rhs_ammo_762x51_Mk316_Special_Ball : rhs_ammo_762x51_M118_Special_Ball
+	{
+		caliber = 1.1;
+	};
+	class rhs_ammo_762x51_M61_AP : rhs_ammo_762x51_M80_Ball
+	{
+		caliber = 1.4;
 	};
 };
