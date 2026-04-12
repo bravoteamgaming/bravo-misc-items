a4f6b86a40931d2c586b7f2d355c83760202bc35
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-04-12 22:00:33 +0100

    Add SU230 variants

--- bravo_gear_misc/cfgWeapons.hpp
+++ bravo_gear_misc/cfgWeapons.hpp
@@ -1,3 +1,19 @@
+class asdg_OpticRail;
+class asdg_OpticRail1913 : asdg_OpticRail
+{
+	class compatibleItems
+	{
+		bravo_rhs_su230a_mrds_ni_3d = 1;
+		bravo_rhs_su230a_mrds_ni = 1;
+		bravo_rhs_su230_mrds_ni_3d = 1;
+		bravo_rhs_su230_mrds_ni = 1;
+		bravo_rhs_su230a_mrds_c_ni_3d = 1;
+		bravo_rhs_su230a_mrds_c_ni = 1;
+		bravo_rhs_su230_mrds_c_ni_3d = 1;
+		bravo_rhs_su230_mrds_c_ni = 1;
+	};
+};
+
 class cfgWeapons
 {
 	class pistol;
@@ -11,6 +27,309 @@ class cfgWeapons
 		{
 			mass = 13.63;
 		};
+	};
+	
+	class rhsusf_acc_su230_base;
+	class rhsusf_acc_su230_mrds_base : rhsusf_acc_su230_base
+	{
+		class ItemInfo;
+	};
+	class rhsusf_acc_su230_mrds : rhsusf_acc_su230_mrds_base
+	{
+		class ItemInfo : ItemInfo
+		{};
+	};
+	class bravo_rhs_su230_mrds_ni : rhsusf_acc_su230_mrds
+	{
+		displayName = "SU-230/PVS MRDS [B]";
+		rhs_optic_base = "bravo_rhs_su230_mrds_ni";
+		class ItemInfo : ItemInfo
+		{
+			class OpticsModes
+			{
+				class Elcan_mrds
+				{
+					cameraDir = "";
+					distanceZoomMax = 200;
+					distanceZoomMin = 200;
+					memoryPointCamera = "eye";
+					opticsDisablePeripherialVision = 0;
+					opticsFlare = 0;
+					opticsID = 3;
+					opticsPPEffects[] = {""};
+					opticsZoomInit = 0.75;
+					opticsZoomMax = 1.1;
+					opticsZoomMin = 0.275;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+				class Elcan_x4
+				{
+					cameraDir = "";
+					discreteDistance[] = {100};
+					discreteDistanceInitIndex = 0;
+					distanceZoomMax = 100;
+					distanceZoomMin = 100;
+					memoryPointCamera = "opticView";
+					opticsDisablePeripherialVision = 1;
+					opticsFlare = 1;
+					opticsID = 2;
+					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1","rhs_su230_Blur"};
+					opticsZoomInit = 0.0625;
+					opticsZoomMax = 0.0625;
+					opticsZoomMin = 0.0625;
+					useModelOptics = 1;
+					visionMode[] = {};
+				};
+				class Elcan_x1
+				{
+					cameraDir = "";
+					distanceZoomMax = 100;
+					distanceZoomMin = 100;
+					memoryPointCamera = "opticview";
+					opticsDisablePeripherialVision = 0;
+					opticsFlare = 0;
+					opticsID = 1;
+					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1","rhs_su230_Blur"};
+					opticsZoomInit = 0.75;
+					opticsZoomMax = 0.6;
+					opticsZoomMin = 0.25;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	class bravo_rhs_su230_mrds_c_ni : bravo_rhs_su230_mrds_ni
+	{
+		displayName = "SU-230/PVS MRDS Coyote [B]";
+		rhs_optic_base = "bravo_rhs_su230_mrds_c_ni";
+		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230_mrds_c";
+		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230_mrds_c_ca.paa";
+	};
+	
+	class rhsusf_acc_su230_mrds_base_3d : rhsusf_acc_su230_mrds_base
+	{
+		class ItemInfo : ItemInfo
+		{};
+	};
+	class rhsusf_acc_su230_mrds_3d : rhsusf_acc_su230_mrds_base_3d
+	{};
+	class bravo_rhs_su230_mrds_ni_3d : rhsusf_acc_su230_mrds_3d
+	{
+		displayName = "SU-230/PVS MRDS [B]";
+		rhs_optic_base = "bravo_rhs_su230_mrds_ni";
+		class ItemInfo : ItemInfo
+		{
+			class OpticsModes
+			{
+				class Elcan_mrds
+				{
+					cameraDir = "";
+					distanceZoomMax = 200;
+					distanceZoomMin = 200;
+					memoryPointCamera = "eye";
+					opticsDisablePeripherialVision = 0;
+					opticsFlare = 0;
+					opticsID = 3;
+					opticsPPEffects[] = {""};
+					opticsZoomInit = 0.75;
+					opticsZoomMax = 1.1;
+					opticsZoomMin = 0.275;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+				class Elcan_x4
+				{
+					cameraDir = "";
+					discreteDistance[] = {100};
+					discreteDistanceInitIndex = 0;
+					distanceZoomMax = 100;
+					distanceZoomMin = 100;
+					memoryPointCamera = "opticView";
+					opticsDisablePeripherialVision = 1;
+					opticsFlare = 1;
+					opticsID = 2;
+					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1","rhs_su230_Blur"};
+					opticsZoomInit = 0.15;
+					opticsZoomMax = 0.15;
+					opticsZoomMin = 0.1;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+				class Elcan_x1
+				{
+					cameraDir = "";
+					distanceZoomMax = 100;
+					distanceZoomMin = 100;
+					memoryPointCamera = "opticview";
+					opticsDisablePeripherialVision = 0;
+					opticsFlare = 0;
+					opticsID = 1;
+					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1","rhs_su230_Blur"};
+					opticsZoomInit = 0.75;
+					opticsZoomMax = 0.6;
+					opticsZoomMin = 0.25;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	class bravo_rhs_su230_mrds_c_ni_3d : bravo_rhs_su230_mrds_ni_3d
+	{
+		displayName = "SU-230/PVS MRDS Coyote [B]";
+		rhs_optic_base = "bravo_rhs_su230_mrds_c_ni";
+		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230_mrds_c";
+		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230_mrds_c_ca.paa";
+	};
+	
+	class rhsusf_acc_su230a_mrds : rhsusf_acc_su230_mrds
+	{
+		class ItemInfo : ItemInfo
+		{};
+	};
+	class bravo_rhs_su230a_mrds_ni : rhsusf_acc_su230a_mrds
+	{
+		displayName = "SU-230A/PVS MRDS [B]";
+		rhs_optic_base = "bravo_rhs_su230a_mrds_ni";
+		class ItemInfo : ItemInfo
+		{
+			class OpticsModes
+			{
+				class Elcan_mrds
+				{
+					cameraDir = "";
+					distanceZoomMax = 200;
+					distanceZoomMin = 200;
+					memoryPointCamera = "eye";
+					opticsDisablePeripherialVision = 0;
+					opticsFlare = 0;
+					opticsID = 3;
+					opticsPPEffects[] = {""};
+					opticsZoomInit = 0.75;
+					opticsZoomMax = 1.1;
+					opticsZoomMin = 0.275;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+				class Elcan_x4
+				{
+					cameraDir = "";
+					discreteDistance[] = {100};
+					discreteDistanceInitIndex = 0;
+					distanceZoomMax = 100;
+					distanceZoomMin = 100;
+					memoryPointCamera = "opticView";
+					opticsDisablePeripherialVision = 1;
+					opticsFlare = 1;
+					opticsID = 2;
+					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1","rhs_su230_Blur"};
+					opticsZoomInit = 0.0625;
+					opticsZoomMax = 0.0625;
+					opticsZoomMin = 0.0625;
+					useModelOptics = 1;
+					visionMode[] = {};
+				};
+				class Elcan_x1
+				{
+					cameraDir = "";
+					distanceZoomMax = 100;
+					distanceZoomMin = 100;
+					memoryPointCamera = "opticview";
+					opticsDisablePeripherialVision = 0;
+					opticsFlare = 0;
+					opticsID = 1;
+					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1","rhs_su230_Blur"};
+					opticsZoomInit = 0.75;
+					opticsZoomMax = 0.6;
+					opticsZoomMin = 0.25;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	class bravo_rhs_su230a_mrds_c_ni : bravo_rhs_su230a_mrds_ni
+	{
+		displayName = "SU-230A/PVS MRDS Coyote [B]";
+		rhs_optic_base = "bravo_rhs_su230a_mrds_c_ni";
+		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230a_mrds_c";
+		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230a_mrds_c_ca.paa";
+	};
+	
+	class rhsusf_acc_su230a_mrds_3d : rhsusf_acc_su230_mrds_3d
+	{
+		class ItemInfo : ItemInfo
+		{};
+	};
+	class bravo_rhs_su230a_mrds_ni_3d : rhsusf_acc_su230a_mrds_3d
+	{
+		displayName = "SU-230A/PVS MRDS [B]";
+		rhs_optic_base = "bravo_rhs_su230a_mrds_ni";
+		class ItemInfo : ItemInfo
+		{
+			class OpticsModes
+			{
+				class Elcan_mrds
+				{
+					cameraDir = "";
+					distanceZoomMax = 200;
+					distanceZoomMin = 200;
+					memoryPointCamera = "eye";
+					opticsDisablePeripherialVision = 0;
+					opticsFlare = 0;
+					opticsID = 3;
+					opticsPPEffects[] = {""};
+					opticsZoomInit = 0.75;
+					opticsZoomMax = 1.1;
+					opticsZoomMin = 0.275;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+				class Elcan_x4
+				{
+					cameraDir = "";
+					discreteDistance[] = {100};
+					discreteDistanceInitIndex = 0;
+					distanceZoomMax = 100;
+					distanceZoomMin = 100;
+					memoryPointCamera = "opticView";
+					opticsDisablePeripherialVision = 1;
+					opticsFlare = 1;
+					opticsID = 2;
+					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1","rhs_su230_Blur"};
+					opticsZoomInit = 0.15;
+					opticsZoomMax = 0.15;
+					opticsZoomMin = 0.1;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+				class Elcan_x1
+				{
+					cameraDir = "";
+					distanceZoomMax = 100;
+					distanceZoomMin = 100;
+					memoryPointCamera = "opticview";
+					opticsDisablePeripherialVision = 0;
+					opticsFlare = 0;
+					opticsID = 1;
+					opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1","rhs_su230_Blur"};
+					opticsZoomInit = 0.75;
+					opticsZoomMax = 0.6;
+					opticsZoomMin = 0.25;
+					useModelOptics = 0;
+					visionMode[] = {};
+				};
+			};
+		};
+	};
+	class bravo_rhs_su230a_mrds_c_ni_3d : bravo_rhs_su230a_mrds_ni_3d
+	{
+		displayName = "SU-230A/PVS MRDS Coyote [B]";
+		rhs_optic_base = "bravo_rhs_su230a_mrds_c_ni";
+		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230a_mrds_c";
+		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230a_mrds_c_ca.paa";
 	};
 	
 	class rhsusf_opscore_fg_pelt;
