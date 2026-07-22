95e06b36c50e6a7e63cce9b7ab59c04ec3b4b791
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-07-22 12:05:15 +0100

    New addon RF AP

---
+++ bravo_rf_ap/cfgMagazines.hpp
@@ -0,0 +1,165 @@
+class cfgMagazines
+{
+	class CA_Magazine;
+	class 20Rnd_556x45_AP_Stanag_RF : CA_Magazine
+	{
+		scopeArsenal = 0;
+	};
+	
+	class rhs_mag_30rnd_556x45_m855a1_stanag;
+	class bravo_30rnd_556x45_hera_m855a1 : rhs_mag_30rnd_556x45_m855a1_stanag
+	{
+		displayName = "5.56 mm 30Rnd Hera M855A1 Magazine";
+		ammo = "rhs_ammo_556x45_m855a1_ball";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_30Rnd_556x45_AP_Stanag_blk_RF_ca.paa";
+	};
+	class bravo_30rnd_556x45_hera_m855a1_snd : bravo_30rnd_556x45_hera_m855a1
+	{
+		displayName = "5.56 mm 30Rnd Hera M855A1 Sand Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_30Rnd_556x45_AP_Stanag_tan_RF_ca.paa";
+	};
+	class bravo_30rnd_556x45_hera_m855a1_oli : bravo_30rnd_556x45_hera_m855a1
+	{
+		displayName = "5.56 mm 30Rnd Hera M855A1 Olive Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_30Rnd_556x45_AP_Stanag_khk_RF_ca.paa";
+	};
+	
+	class bravo_20rnd_556x45_hera_m855a1 : bravo_30rnd_556x45_hera_m855a1
+	{
+		displayName = "5.56 mm 20Rnd Hera M855A1 Magazine";
+		mass = 6.3;
+		count = 20;
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_20Rnd_556x45_AP_Stanag_blk_RF_ca.paa";
+	};
+	class bravo_20rnd_556x45_hera_m855a1_snd : bravo_20rnd_556x45_hera_m855a1
+	{
+		displayName = "5.56 mm 20Rnd Hera M855A1 Sand Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_20Rnd_556x45_AP_Stanag_tan_RF_ca.paa";
+	};
+	class bravo_20rnd_556x45_hera_m855a1_oli : bravo_20rnd_556x45_hera_m855a1
+	{
+		displayName = "5.56 mm 20Rnd Hera M855A1 Olive Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_20Rnd_556x45_AP_Stanag_khk_RF_ca.paa";
+	};
+	
+	class bravo_10rnd_556x45_hera_m855a1 : bravo_30rnd_556x45_hera_m855a1
+	{
+		displayName = "5.56 mm 10Rnd Hera M855A1 Magazine";
+		mass = 3.3;
+		count = 10;
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_10Rnd_556x45_AP_Stanag_blk_RF_ca.paa";
+	};
+	class bravo_10rnd_556x45_hera_m855a1_snd : bravo_10rnd_556x45_hera_m855a1
+	{
+		displayName = "5.56 mm 10Rnd Hera M855A1 Sand Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_10Rnd_556x45_AP_Stanag_tan_RF_ca.paa";
+	};
+	class bravo_10rnd_556x45_hera_m855a1_oli : bravo_10rnd_556x45_hera_m855a1
+	{
+		displayName = "5.56 mm 10Rnd Hera M855A1 Olive Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_10Rnd_556x45_AP_Stanag_khk_RF_ca.paa";
+	};
+	
+	
+	class rhs_mag_30rnd_556x45_m856a1_stanag;
+	class bravo_30rnd_556x45_hera_m856a1 : rhs_mag_30rnd_556x45_m856a1_stanag
+	{
+		displayName = "5.56 mm 30Rnd Hera M856A1 (Tracer) Magazine";
+		ammo = "rhs_ammo_556x45_m856a1_ball_red";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_30Rnd_556x45_AP_Stanag_blk_RF_ca.paa";
+	};
+	class bravo_30rnd_556x45_hera_m856a1_snd : bravo_30rnd_556x45_hera_m856a1
+	{
+		displayName = "5.56 mm 30Rnd Hera M856A1 (Tracer) Sand Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_30Rnd_556x45_AP_Stanag_tan_RF_ca.paa";
+	};
+	class bravo_30rnd_556x45_hera_m856a1_oli : bravo_30rnd_556x45_hera_m856a1
+	{
+		displayName = "5.56 mm 30Rnd Hera M856A1 (Tracer) Olive Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_30Rnd_556x45_AP_Stanag_khk_RF_ca.paa";
+	};
+	
+	class bravo_20rnd_556x45_hera_m856a1 : bravo_30rnd_556x45_hera_m856a1
+	{
+		displayName = "5.56 mm 20Rnd Hera M856A1 (Tracer) Magazine";
+		mass = 6.3;
+		count = 20;
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_20Rnd_556x45_AP_Stanag_blk_RF_ca.paa";
+	};
+	class bravo_20rnd_556x45_hera_m856a1_snd : bravo_20rnd_556x45_hera_m856a1
+	{
+		displayName = "5.56 mm 20Rnd Hera M856A1 (Tracer) Sand Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_20Rnd_556x45_AP_Stanag_tan_RF_ca.paa";
+	};
+	class bravo_20rnd_556x45_hera_m856a1_oli : bravo_20rnd_556x45_hera_m856a1
+	{
+		displayName = "5.56 mm 20Rnd Hera M856A1 (Tracer) Olive Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_20Rnd_556x45_AP_Stanag_khk_RF_ca.paa";
+	};
+	
+	class bravo_10rnd_556x45_hera_m856a1 : bravo_30rnd_556x45_hera_m856a1
+	{
+		displayName = "5.56 mm 10Rnd Hera M856A1 (Tracer) Magazine";
+		mass = 3.3;
+		count = 10;
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_10Rnd_556x45_AP_Stanag_blk_RF_ca.paa";
+	};
+	class bravo_10rnd_556x45_hera_m856a1_snd : bravo_10rnd_556x45_hera_m856a1
+	{
+		displayName = "5.56 mm 10Rnd Hera M856A1 (Tracer) Sand Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_10Rnd_556x45_AP_Stanag_tan_RF_ca.paa";
+	};
+	class bravo_10rnd_556x45_hera_m856a1_oli : bravo_10rnd_556x45_hera_m856a1
+	{
+		displayName = "5.56 mm 10Rnd Hera M856A1 (Tracer) Olive Magazine";
+		picture = "\lxRF\weapons_rf\Rifles\H6\data\ui\icon_10Rnd_556x45_AP_Stanag_khk_RF_ca.paa";
+	};
+};
+
+class cfgMagazineWells
+{
+	class CBA_556x45_STANAG
+	{
+		bravo_rf_mags[] = {
+			"bravo_30rnd_556x45_hera_m855a1",
+			"bravo_30rnd_556x45_hera_m855a1_oli",
+			"bravo_30rnd_556x45_hera_m855a1_snd",
+			"bravo_20rnd_556x45_hera_m855a1",
+			"bravo_20rnd_556x45_hera_m855a1_oli",
+			"bravo_20rnd_556x45_hera_m855a1_snd",
+			"bravo_10rnd_556x45_hera_m855a1",
+			"bravo_10rnd_556x45_hera_m855a1_oli",
+			"bravo_10rnd_556x45_hera_m855a1_snd",
+			"bravo_30rnd_556x45_hera_m856a1",
+			"bravo_30rnd_556x45_hera_m856a1_oli",
+			"bravo_30rnd_556x45_hera_m856a1_snd",
+			"bravo_20rnd_556x45_hera_m856a1",
+			"bravo_20rnd_556x45_hera_m856a1_oli",
+			"bravo_20rnd_556x45_hera_m856a1_snd",
+			"bravo_10rnd_556x45_hera_m856a1",
+			"bravo_10rnd_556x45_hera_m856a1_oli",
+			"bravo_10rnd_556x45_hera_m856a1_snd"};
+	};
+	class STANAG_556x45
+	{
+		bravo_rf_mags[] = {
+			"bravo_30rnd_556x45_hera_m855a1",
+			"bravo_30rnd_556x45_hera_m855a1_oli",
+			"bravo_30rnd_556x45_hera_m855a1_snd",
+			"bravo_20rnd_556x45_hera_m855a1",
+			"bravo_20rnd_556x45_hera_m855a1_oli",
+			"bravo_20rnd_556x45_hera_m855a1_snd",
+			"bravo_10rnd_556x45_hera_m855a1",
+			"bravo_10rnd_556x45_hera_m855a1_oli",
+			"bravo_10rnd_556x45_hera_m855a1_snd",
+			"bravo_30rnd_556x45_hera_m856a1",
+			"bravo_30rnd_556x45_hera_m856a1_oli",
+			"bravo_30rnd_556x45_hera_m856a1_snd",
+			"bravo_20rnd_556x45_hera_m856a1",
+			"bravo_20rnd_556x45_hera_m856a1_oli",
+			"bravo_20rnd_556x45_hera_m856a1_snd",
+			"bravo_10rnd_556x45_hera_m856a1",
+			"bravo_10rnd_556x45_hera_m856a1_oli",
+			"bravo_10rnd_556x45_hera_m856a1_snd"};
+	};
+};
