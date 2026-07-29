31ab84e7fad766bbb1a1c6ee5155ba5bfdf67813
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-07-29 15:16:13 +0100

    magwell fixes

--- bravo_qbzak/cfgMagazines.hpp
+++ bravo_qbzak/cfgMagazines.hpp
@@ -1,15 +1,18 @@
-class 75rnd_762x39_AK12_Mag_F;
-class bravo_75rnd_762x39mm_qjb95_mag : 75rnd_762x39_AK12_Mag_F
+class cfgMagazines
 {
-	displayName = "7.62x39 mm 75Rnd QJB-95 Magazine";
-	hiddenSelections[] = {"Camo"};
-	hiddenSelectionsTextures[] = {"a3\Weapons_F_Exp\Rifles\CTARS\Data\CTARS_F_co.paa"};
-	modelSpecial = "a3\Weapons_F_exp\MagazineProxies\mag_58x42_CTAR_100rnd";
-};
-class bravo_75rnd_762x39mm_qjb95_tracer_mag : bravo_75rnd_762x39mm_qjb95_mag
-{
-	displayName = "7.62x39 mm 75Rnd QJB-95 Tracer Magazine";
-	tracersEvery = 1;
+	class 75rnd_762x39_AK12_Mag_F;
+	class bravo_75rnd_762x39mm_qjb95_mag : 75rnd_762x39_AK12_Mag_F
+	{
+		displayName = "7.62x39 mm 75Rnd QJB-95 Magazine";
+		hiddenSelections[] = {"Camo"};
+		hiddenSelectionsTextures[] = {"a3\Weapons_F_Exp\Rifles\CTARS\Data\CTARS_F_co.paa"};
+		modelSpecial = "a3\Weapons_F_exp\MagazineProxies\mag_58x42_CTAR_100rnd";
+	};
+	class bravo_75rnd_762x39mm_qjb95_tracer_mag : bravo_75rnd_762x39mm_qjb95_mag
+	{
+		displayName = "7.62x39 mm 75Rnd QJB-95 Tracer Magazine";
+		tracersEvery = 1;
+	};
 };
 
 class cfgMagazineWells
