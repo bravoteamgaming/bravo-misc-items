e60adbbba85cb3cea6ce7ac0923a5273eed4d6cc
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-07-29 01:09:54 +0100

    new addon QBZAK

---
+++ bravo_qbzak/cfgWeapons.hpp
@@ -0,0 +1,183 @@
+class CfgWeapons
+{	
+
+	class AK12_F;
+	class AK12_GL_F;
+	class AK12U_F;
+	class RPK12_F;
+	
+	class bravo_type215 : AK12_F
+	{
+		baseWeapon = "bravo_type215";
+		displayName = "Type 215";
+		descriptionShort = "Assault rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_gl : AK12_GL_F
+	{
+		baseWeapon = "bravo_type215_gl";
+		displayName = "Type 215 GL";
+		descriptionShort = "Assault rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_k : AK12_F
+	{
+		baseWeapon = "bravo_type215_k";
+		displayName = "Type 215 Carbine";
+		descriptionShort = "Carbine<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_lsw : AK12_F
+	{
+		baseWeapon = "bravo_type215_lsw";
+		displayName = "Type 215 LSW";
+		descriptionShort = "Automatic rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"100Rnd_580x42_Mag_F","100Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar_lsw";
+	};
+	
+	class arifle_AK12_arid_F;
+	class arifle_AK12_GL_arid_F;
+	class arifle_AK12U_arid_F;
+	class arifle_RPK12_arid_F;
+	
+	class bravo_type215_arid : arifle_AK12_arid_F
+	{
+		baseWeapon = "bravo_type215_arid";
+		displayName = "Type 215 (Arid)";
+		descriptionShort = "Assault rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_gl_arid : arifle_AK12_GL_arid_F
+	{
+		baseWeapon = "bravo_type215_gl_arid";
+		displayName = "Type 215 GL (Arid)";
+		descriptionShort = "Assault rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_k_arid : arifle_AK12U_arid_F
+	{
+		baseWeapon = "bravo_type215_k_arid";
+		displayName = "Type 215 Carbine (Arid)";
+		descriptionShort = "Carbine<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_lsw_arid : arifle_RPK12_arid_F
+	{
+		baseWeapon = "bravo_type215_lsw_arid";
+		displayName = "Type 215 LSW (Arid)";
+		descriptionShort = "Automatic rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"100Rnd_580x42_Mag_F","100Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar_lsw";
+	};
+	
+	class arifle_AK12_lush_F;
+	class arifle_AK12_GL_lush_F;
+	class arifle_AK12U_lush_F;
+	class arifle_RPK12_lush_F;
+	
+	class bravo_type215_lush : arifle_AK12_lush_F
+	{
+		baseWeapon = "bravo_type215_lush";
+		displayName = "Type 215 (Lush)";
+		descriptionShort = "Assault rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_gl_lush : arifle_AK12_GL_lush_F
+	{
+		baseWeapon = "bravo_type215_gl_lush";
+		displayName = "Type 215 GL (Lush)";
+		descriptionShort = "Assault rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_k_lush : arifle_AK12U_lush_F
+	{
+		baseWeapon = "bravo_type215_k_lush";
+		displayName = "Type 215 Carbine (Lush)";
+		descriptionShort = "Carbine<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"30Rnd_580x42_Mag_F","30Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar";
+	};
+	class bravo_type215_lsw_lush : arifle_RPK12_lush_F
+	{
+		baseWeapon = "bravo_type215_lsw_lush";
+		displayName = "Type 215 LSW (Lush)";
+		descriptionShort = "Automatic rifle<br />Caliber: 5.8x42 mm";
+		magazineWell[] = {"CTAR_580x42","CTAR_580x42_Large","CBA_580x42_TYPE95","CBA_580x42_TYPE95_XL"};
+		magazines[] = {"100Rnd_580x42_Mag_F","100Rnd_580x42_Mag_Tracer_F"};
+		recoil = "recoil_spar_lsw";
+	};
+	
+	class arifle_CTAR_blk_F;
+	class arifle_CTAR_GL_blk_F;
+	class arifle_CTARS_blk_F;
+	
+	class bravo_qbz95_545 : arifle_CTAR_blk_F
+	{
+		baseWeapon = "bravo_qbz95_545";
+		displayName = "QBZ-95-545";
+		descriptionShort = "Assault rifle<br />Caliber: 5.45x39 mm";
+		magazineWell[] = {"AK_545x39","CBA_545x39_AK","CBA_545x39_RPK"};
+		magazines[] = {"rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_plum_AK","rhs_30Rnd_545x39_7N10_desert_AK","rhs_30Rnd_545x39_7N10_camo_AK"};
+	};
+	class bravo_qbz95_545_gl : arifle_CTAR_GL_blk_F
+	{
+		baseWeapon = "bravo_qbz95_545_gl";
+		displayName = "QBZ-95-545 GL";
+		descriptionShort = "Assault rifle<br />Caliber: 5.45x39 mm";
+		magazineWell[] = {"AK_545x39","CBA_545x39_AK","CBA_545x39_RPK"};
+		magazines[] = {"rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_plum_AK","rhs_30Rnd_545x39_7N10_desert_AK","rhs_30Rnd_545x39_7N10_camo_AK"};
+	};
+	class bravo_qjb95_545 : arifle_CTARS_blk_F
+	{
+		baseWeapon = "bravo_qjb95_545";
+		displayName = "QJB-95-545";
+		descriptionShort = "Automatic rifle<br />Caliber: 5.45x39 mm";
+		magazineWell[] = {"AK_545x39","CBA_545x39_AK","CBA_545x39_RPK"};
+		magazines[] = {"rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_plum_AK","rhs_30Rnd_545x39_7N10_desert_AK","rhs_30Rnd_545x39_7N10_camo_AK"};
+	};
+	
+	class bravo_qbz95_762 : arifle_CTAR_blk_F
+	{
+		baseWeapon = "bravo_qbz95_762";
+		displayName = "QBZ-95-762";
+		descriptionShort = "Assault rifle<br />Caliber: 7.62x39 mm";
+		magazines[] = {"rhs_30Rnd_762x39mm_bakelite","rhs_30Rnd_762x39mm_bakelite_tracer","rhs_30Rnd_762x39mm_bakelite_89","rhs_30Rnd_762x39mm_bakelite_U"};
+		magazineWell[] = {"AK_762x39","CBA_762x39_AK"};
+	};
+	class bravo_qbz95_762_gl : arifle_CTAR_GL_blk_F
+	{
+		baseWeapon = "bravo_qbz95_762_gl";
+		displayName = "QBZ-95-762 GL";
+		descriptionShort = "Assault rifle<br />Caliber: 7.62x39 mm";
+		magazines[] = {"rhs_30Rnd_762x39mm_bakelite","rhs_30Rnd_762x39mm_bakelite_tracer","rhs_30Rnd_762x39mm_bakelite_89","rhs_30Rnd_762x39mm_bakelite_U"};
+		magazineWell[] = {"AK_762x39","CBA_762x39_AK"};
+	};
+	class bravo_qjb95_762 : arifle_CTARS_blk_F
+	{
+		baseWeapon = "bravo_qjb95_762";
+		displayName = "QJB-95-762";
+		descriptionShort = "Automatic rifle<br />Caliber: 7.62x39 mm";
+		magazines[] = {"rhs_30Rnd_762x39mm_bakelite","rhs_30Rnd_762x39mm_bakelite_tracer","rhs_30Rnd_762x39mm_bakelite_89","rhs_30Rnd_762x39mm_bakelite_U"};
+		magazineWell[] = {"AK_762x39","CBA_762x39_AK"};
+	};
+};
