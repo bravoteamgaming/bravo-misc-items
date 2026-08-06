17056ff6a8d683491224b6600ccff8220f0c40cc
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-08-06 17:53:38 +0100

    brown SOV

--- bravo_gear_misc/cfgWeapons/cfgWeapons.hpp
+++ bravo_gear_misc/cfgWeapons/cfgWeapons.hpp
@@ -615,29 +615,7 @@ class cfgWeapons
 		};
 	};
 	
-	class gm_ge_vest_sov_80_oli;
-	class bravo_gm_vest_sov_des : gm_ge_vest_sov_80_oli
-	{
-		displayName = "Special Operations Vest (Tan)";
-		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_des_co"};
-	};
-	class bravo_gm_vest_sov_khk : gm_ge_vest_sov_80_oli
-	{
-		displayName = "Special Operations Vest (Khaki)";
-		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_khk_co"};
-	};
-	
-	class gm_ge_vest_sov_armor_80_oli;
-	class bravo_gm_vest_sov_armor_des : gm_ge_vest_sov_armor_80_oli
-	{
-		displayName = "Special Operations Vest (Armor, Tan)";
-		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_des_co", "\gm\gm_characters\gm_ge_characters\data\vest\gm_ge_vest_type3_oli_co"};
-	};
-	class bravo_gm_vest_sov_armor_khk : gm_ge_vest_sov_armor_80_oli
-	{
-		displayName = "Special Operations Vest (Armor, Khaki)";
-		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_khk_co", "\gm\gm_characters\gm_ge_characters\data\vest\gm_ge_vest_type3_oli_co"};
-	};
+	#include "gmsov.hpp"
 	
 	#include "boonie.hpp"
 	
---
+++ bravo_gear_misc/cfgWeapons/gmsov.hpp
@@ -0,0 +1,33 @@
+	class gm_ge_vest_sov_80_oli;
+	class bravo_gm_vest_sov_des : gm_ge_vest_sov_80_oli
+	{
+		displayName = "Special Operations Vest (Tan)";
+		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_des_co"};
+	};
+	class bravo_gm_vest_sov_khk : gm_ge_vest_sov_80_oli
+	{
+		displayName = "Special Operations Vest (Khaki)";
+		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_khk_co"};
+	};
+	class bravo_gm_vest_sov_brn : gm_ge_vest_sov_80_oli
+	{
+		displayName = "Special Operations Vest (Brown)";
+		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_brn_co"};
+	};
+	
+	class gm_ge_vest_sov_armor_80_oli;
+	class bravo_gm_vest_sov_armor_des : gm_ge_vest_sov_armor_80_oli
+	{
+		displayName = "Special Operations Vest (Armor, Tan)";
+		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_des_co", "\gm\gm_characters\gm_ge_characters\data\vest\gm_ge_vest_type3_oli_co"};
+	};
+	class bravo_gm_vest_sov_armor_khk : gm_ge_vest_sov_armor_80_oli
+	{
+		displayName = "Special Operations Vest (Armor, Khaki)";
+		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_khk_co", "\gm\gm_characters\gm_ge_characters\data\vest\gm_ge_vest_type3_oli_co"};
+	};
+	class bravo_gm_vest_sov_armor_brn : gm_ge_vest_sov_armor_80_oli
+	{
+		displayName = "Special Operations Vest (Armor, Brown)";
+		hiddenSelectionsTextures[] = {"\bravo_gear_misc\assets\bravo_gm_vest_sov_brn_co", "\gm\gm_characters\gm_ge_characters\data\vest\gm_ge_vest_type3_oli_co"};
+	};
