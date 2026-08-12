c0989105ee6b0cd8a633c03ce1a2b76574031a89
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-08-12 23:11:14 +0100

    typo

--- bravo_cba_rails_vanilla/config.cpp
+++ bravo_cba_rails_vanilla/config.cpp
@@ -22,7 +22,7 @@ class cfgMagazineWells
 		bravo_cba_rails_vanilla_mags[] = {
 			"150Rnd_556x45_Drum_Green_Mag_F",
 			"150Rnd_556x45_Drum_Sand_Mag_F",
-			"150Rnd_556x45_Drum_Green_Mag_TracerF",
+			"150Rnd_556x45_Drum_Green_Mag_Tracer_F",
 			"150Rnd_556x45_Drum_Sand_Mag_Tracer_F"
 		};
 	};
