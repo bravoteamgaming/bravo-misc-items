5db870168743de8c3d25dccb399cb0b035838568
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-09-11 08:07:26 +0100

    adjust volumes

--- bravo_items/cfgSFX.hpp
+++ bravo_items/cfgSFX.hpp
@@ -2,14 +2,14 @@ class CfgSFX
 {
 	class bravo_alarmImperial
 	{
-		sound0[] = { "\bravo_items\assets\alarm_imperial.ogg", db-13, 1.0, 500, 1, 3, 3, 3 };
+		sound0[] = { "\bravo_items\assets\alarm_imperial.ogg", db-18, 1.0, 500, 1, 3, 3, 3 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Alarm (Imperial)";
 	};
 	class bravo_alarmIncoming
 	{
-		sound0[] = { "\bravo_items\assets\alarm_incoming.ogg", db-13, 1.0, 500, 1, 1, 1, 1 };
+		sound0[] = { "\bravo_items\assets\alarm_incoming.ogg", db-15, 1.0, 500, 1, 1, 1, 1 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Alarm (Incoming)";
@@ -30,56 +30,56 @@ class CfgSFX
 	};
 	class bravo_alarmVanilla
 	{
-		sound0[] = { "A3\Sounds_F\sfx\alarm_opfor", db-13, 1.0, 500, 1, 2, 2, 2 };
+		sound0[] = { "A3\Sounds_F\sfx\alarm_opfor", db-15, 1.0, 500, 1, 2, 2, 2 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Alarm (Vanilla)";
 	};
 	class bravo_alarmStarfleet
 	{
-		sound0[] = { "\bravo_items\assets\alarm_starfleet.ogg", db-13, 1.0, 500, 1, 2, 2, 2 };
+		sound0[] = { "\bravo_items\assets\alarm_starfleet.ogg", db-15, 1.0, 500, 1, 2, 2, 2 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Alarm (Vanilla)";
 	};
 	class bravo_radioMuitinkinyakkin
 	{
-		sound0[] = { "\bravo_items\assets\radio_muitinkinyakkin.ogg", db-9, 1.0, 100, 1, 30, 30, 30 };
+		sound0[] = { "\bravo_items\assets\radio_muitinkinyakkin.ogg", db-11, 1.0, 100, 1, 30, 30, 30 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Radio (Muitinkinyakkin)";
 	};
 	class bravo_radioLithiumFlower
 	{
-		sound0[] = { "\bravo_items\assets\radio_lithiumflower.ogg", db-9, 1.0, 100, 1, 30, 30, 30 };
+		sound0[] = { "\bravo_items\assets\radio_lithiumflower.ogg", db-10, 1.0, 100, 1, 30, 30, 30 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Radio (Lithium Flower)";
 	};
 	class bravo_alarmCar
 	{
-		sound0[] = { "\bravo_items\assets\alarm_car.ogg", db-13, 1.0, 300, 1, 0, 0, 0 };
+		sound0[] = { "\bravo_items\assets\alarm_car.ogg", db-16, 1.0, 300, 1, 0, 0, 0 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Alarm (Car)";
 	};
 	class bravo_radioPumpUpTheJam
 	{
-		sound0[] = { "\bravo_items\assets\radio_pumpupthejam.ogg", db-9, 1, 100, 1, 30, 30, 30 };
+		sound0[] = { "\bravo_items\assets\radio_pumpupthejam.ogg", db-11, 1, 100, 1, 30, 30, 30 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Radio (Pump Up The Jam)";
 	};
 	class bravo_radioHalfLife
 	{
-		sound0[] = { "\bravo_items\assets\radio_halflife.ogg", db-9, 1.0, 100, 1, 30, 30, 30 };
+		sound0[] = { "\bravo_items\assets\radio_halflife.ogg", db-11, 1.0, 100, 1, 30, 30, 30 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Radio (Half-Life)";
 	};
 	class bravo_radioPiano
 	{
-		sound0[] = { "\bravo_items\assets\radio_piano.ogg", db-9, 1.0, 100, 1, 30, 30, 30 };
+		sound0[] = { "\bravo_items\assets\radio_piano.ogg", db-11, 1.0, 100, 1, 30, 30, 30 };
 		sounds[] = { "sound0"};
 		empty[] = { "", 0, 0, 0, 0, 0, 0, 0 };
 		name = "Radio (Piano)";
