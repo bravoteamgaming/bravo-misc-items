643c26f0fcd54ba110961b79786cab1e2efbe7b5
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-09-18 13:23:10 +0100

    SU230 swapping

--- bravo_gear_misc/cfgWeapons/su230.hpp
+++ bravo_gear_misc/cfgWeapons/su230.hpp
@@ -6,6 +6,9 @@
 	};
 	class rhsusf_acc_su230_mrds : rhsusf_acc_su230_mrds_base
 	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230_mrds_ni";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230_mrds_ni";
+		MTR_switchItemHintText = "Irons";
 		class ItemInfo : ItemInfo
 		{};
 	};
@@ -13,6 +16,9 @@
 	{
 		displayName = "SU-230/PVS MRDS [B]";
 		rhs_optic_base = "bravo_rhs_su230_mrds_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230_mrds";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230_mrds";
+		MTR_switchItemHintText = "No irons";
 		class ItemInfo : ItemInfo
 		{
 			class OpticsModes
@@ -74,9 +80,19 @@
 	{
 		displayName = "SU-230/PVS MRDS Coyote [B]";
 		rhs_optic_base = "bravo_rhs_su230_mrds_c_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230_mrds_c";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230_mrds_c";
+		MTR_switchItemHintText = "Irons";
 		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230_mrds_c";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230_mrds_c_ca.paa";
 		weaponInfoType = "rhsusf_acc_su230_mrds_c_handler";
+	};
+	
+	class rhsusf_acc_su230_mrds_c : rhsusf_acc_su230_mrds_base
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230_mrds_c_ni";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230_mrds_c_ni";
+		MTR_switchItemHintText = "Irons";
 	};
 	
 	class rhsusf_acc_su230_mrds_base_3d : rhsusf_acc_su230_mrds_base
@@ -85,11 +101,18 @@
 		{};
 	};
 	class rhsusf_acc_su230_mrds_3d : rhsusf_acc_su230_mrds_base_3d
-	{};
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230_mrds_ni_3d";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230_mrds_ni_3d";
+		MTR_switchItemHintText = "Irons";
+	};
 	class bravo_rhs_su230_mrds_ni_3d : rhsusf_acc_su230_mrds_3d
 	{
 		displayName = "SU-230/PVS MRDS [B]";
 		rhs_optic_base = "bravo_rhs_su230_mrds_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230_mrds_3d";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230_mrds_3d";
+		MTR_switchItemHintText = "No irons";
 		class ItemInfo : ItemInfo
 		{
 			class OpticsModes
@@ -151,13 +174,26 @@
 	{
 		displayName = "SU-230/PVS MRDS Coyote [B]";
 		rhs_optic_base = "bravo_rhs_su230_mrds_c_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230_mrds_c_3d";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230_mrds_c_3d";
+		MTR_switchItemHintText = "No irons";
 		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230_mrds_c";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230_mrds_c_ca.paa";
 		weaponInfoType = "rhsusf_acc_su230_mrds_c_3d_handler";
 	};
 	
+	class rhsusf_acc_su230_mrds_c_3d : rhsusf_acc_su230_mrds_base_3d
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230_mrds_c_ni_3d";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230_mrds_c_ni_3d";
+		MTR_switchItemHintText = "Irons";
+	};
+	
 	class rhsusf_acc_su230a_mrds : rhsusf_acc_su230_mrds
 	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230a_mrds_ni";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230a_mrds_ni";
+		MTR_switchItemHintText = "Irons";
 		class ItemInfo : ItemInfo
 		{};
 	};
@@ -165,6 +201,9 @@
 	{
 		displayName = "SU-230A/PVS MRDS [B]";
 		rhs_optic_base = "bravo_rhs_su230a_mrds_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230a_mrds";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230a_mrds";
+		MTR_switchItemHintText = "No rons";
 		class ItemInfo : ItemInfo
 		{
 			class OpticsModes
@@ -226,13 +265,26 @@
 	{
 		displayName = "SU-230A/PVS MRDS Coyote [B]";
 		rhs_optic_base = "bravo_rhs_su230a_mrds_c_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230a_mrds_c";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230a_mrds_c";
+		MTR_switchItemHintText = "No irons";
 		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230a_mrds_c";
 		weaponInfoType = "rhsusf_acc_su230a_mrds_c_handler";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230a_mrds_c_ca.paa";
 	};
 	
+	class rhsusf_acc_su230a_mrds_c : rhsusf_acc_su230a_mrds
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230a_mrds_c_ni";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230a_mrds_c_ni";
+		MTR_switchItemHintText = "Irons";
+	};
+	
 	class rhsusf_acc_su230a_mrds_3d : rhsusf_acc_su230_mrds_3d
 	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230a_mrds_ni_3d";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230a_mrds_ni_3d";
+		MTR_switchItemHintText = "Irons";
 		class ItemInfo : ItemInfo
 		{};
 	};
@@ -240,6 +292,9 @@
 	{
 		displayName = "SU-230A/PVS MRDS [B]";
 		rhs_optic_base = "bravo_rhs_su230a_mrds_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230a_mrds_3d";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230a_mrds_3d";
+		MTR_switchItemHintText = "No irons";
 		class ItemInfo : ItemInfo
 		{
 			class OpticsModes
@@ -301,19 +356,35 @@
 	{
 		displayName = "SU-230A/PVS MRDS Coyote [B]";
 		rhs_optic_base = "bravo_rhs_su230a_mrds_c_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230a_mrds_c_3d";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230a_mrds_c_3d";
+		MTR_switchItemHintText = "No irons";
 		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230a_mrds_c";
 		weaponInfoType = "rhsusf_acc_su230a_mrds_c_3d_handler";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230a_mrds_c_ca.paa";
 	};
 	
+	class rhsusf_acc_su230a_mrds_c_3d : rhsusf_acc_su230a_mrds_3d
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230a_mrds_c_ni_3d";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230a_mrds_c_ni_3d";
+		MTR_switchItemHintText = "Irons";
+	};
+	
 	class rhsusf_acc_su230 : rhsusf_acc_su230_base
 	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230_ni";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230_ni";
+		MTR_switchItemHintText = "Irons";
 		class ItemInfo;
 	};
 	class bravo_rhs_su230_ni : rhsusf_acc_su230
 	{
 		displayName = "SU-230/PVS [B]";
 		rhs_optic_base = "bravo_rhs_su230_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230";
+		MTR_switchItemHintText = "No irons";
 		class ItemInfo : ItemInfo
 		{
 			class OpticsModes
@@ -359,9 +430,20 @@
 	{
 		displayName = "SU-230/PVS Coyote [B]";
 		rhs_optic_base = "bravo_rhs_su230_c_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230_c";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230_c";
+		MTR_switchItemHintText = "No irons";
 		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230_c";
 		weaponInfoType = "rhsusf_acc_su230_c_handler";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230_c_ca.paa";
+	};
+	
+	class rhsusf_acc_su230_c : rhsusf_acc_su230_base
+	{
+		
+		MRT_SwitchItemNextClass = "bravo_rhs_su230_c_ni";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230_c_ni";
+		MTR_switchItemHintText = "Irons";
 	};
 	
 	class rhsusf_acc_su230_base_3d : rhsusf_acc_su230_base
@@ -369,11 +451,20 @@
 		class ItemInfo;
 	};
 	class rhsusf_acc_su230_3d : rhsusf_acc_su230_base_3d
-	{};
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230_ni_3d";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230_ni_3d";
+		MTR_switchItemHintText = "Irons";
+		class ItemInfo;
+	};
 	class bravo_rhs_su230_ni_3d : rhsusf_acc_su230_3d
 	{
 		displayName = "SU-230/PVS [B]";
 		rhs_optic_base = "bravo_rhs_su230_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230_3d";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230_3d";
+		MTR_switchItemHintText = "No irons";
+		class ItemInfo;
 		class ItemInfo : ItemInfo
 		{
 			class OpticsModes
@@ -419,13 +510,26 @@
 	{
 		displayName = "SU-230/PVS Coyote [B]";
 		rhs_optic_base = "bravo_rhs_su230_c_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230_c_3d";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230_c_3d";
+		MTR_switchItemHintText = "No irons";
 		weaponInfoType = "rhsusf_acc_su230_c_3d_handler";
 		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230_c";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230_c_ca.paa";
 	};
 	
+	class rhsusf_acc_su230_c_3d : rhsusf_acc_su230_base_3d
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230_c_ni_3d";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230_c_ni_3d";
+		MTR_switchItemHintText = "Irons";
+	};
+	
 	class rhsusf_acc_su230a : rhsusf_acc_su230
 	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230a_ni";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230a_ni";
+		MTR_switchItemHintText = "Irons";
 		class ItemInfo : ItemInfo
 		{};
 	};
@@ -433,6 +537,9 @@
 	{
 		displayName = "SU-230A/PVS [B]";
 		rhs_optic_base = "bravo_rhs_su230a_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230a";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230a";
+		MTR_switchItemHintText = "No irons";
 		class ItemInfo : ItemInfo
 		{
 			class OpticsModes
@@ -478,13 +585,26 @@
 	{
 		displayName = "SU-230A/PVS Coyote [B]";
 		rhs_optic_base = "bravo_rhs_su230a_c_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230a_c";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230a_c";
+		MTR_switchItemHintText = "No irons";
 		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230a_c";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230a_c_ca.paa";
 		weaponInfoType = "rhsusf_acc_su230a_c_handler";
 	};
 	
+	class rhsusf_acc_su230a_c : rhsusf_acc_su230a
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230a_c_ni";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230a_c_ni";
+		MTR_switchItemHintText = "Irons";
+	};
+	
 	class rhsusf_acc_su230a_3d : rhsusf_acc_su230_3d
 	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230a_ni_3d";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230a_ni_3d";
+		MTR_switchItemHintText = "Irons";
 		class ItemInfo : ItemInfo
 		{};
 	};
@@ -492,6 +612,9 @@
 	{
 		displayName = "SU-230A/PVS [B]";
 		rhs_optic_base = "bravo_rhs_su230a_ni";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230a_3d";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230a_3d";
+		MTR_switchItemHintText = "No irons";
 		class ItemInfo : ItemInfo
 		{
 			class OpticsModes
@@ -537,7 +660,17 @@
 	{
 		displayName = "SU-230A/PVS Coyote [B]";
 		weaponInfoType = "rhsusf_acc_su230a_c_3d_handler";
+		MRT_SwitchItemNextClass = "rhsusf_acc_su230a_c_3d";
+		MRT_SwitchItemPrevClass = "rhsusf_acc_su230a_c_3d";
+		MTR_switchItemHintText = "No irons";
 		rhs_optic_base = "bravo_rhs_su230a_c_ni";
 		model = "\rhsusf\addons\rhsusf_scopes\specter\rhs_su230a_c";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\accessories\rhsusf_acc_su230a_c_ca.paa";
+	};
+	
+	class rhsusf_acc_su230a_c_3d : rhsusf_acc_su230a_3d
+	{
+		MRT_SwitchItemNextClass = "bravo_rhs_su230a_c_ni_3d";
+		MRT_SwitchItemPrevClass = "bravo_rhs_su230a_c_ni_3d";
+		MTR_switchItemHintText = "Irons";
 	};
