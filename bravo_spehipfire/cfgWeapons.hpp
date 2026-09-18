ad32b01c8049f7d8bfcb611a325908171ef01816
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-09-18 18:47:23 +0100

    swap system

--- bravo_spehipfire/cfgWeapons.hpp
+++ bravo_spehipfire/cfgWeapons.hpp
@@ -39,7 +39,7 @@ class CfgWeapons
 	};
 	class uk3cb_hs50 : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldM107";
+		SPE_deployment_animation = "RHS_GestureHoldM107";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -52,7 +52,7 @@ class CfgWeapons
 	};
 	class mmg_01_base_f : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldM240";
+		SPE_deployment_animation = "RHS_GestureHoldM240";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -65,7 +65,7 @@ class CfgWeapons
 	};
 	class mmg_02_base_f : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldM240";
+		SPE_deployment_animation = "RHS_GestureHoldM240";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -78,7 +78,7 @@ class CfgWeapons
 	};
 	class lrr_base_f : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldMG42";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -91,7 +91,7 @@ class CfgWeapons
 	};
 	class uk3cb_baf_l115_base : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 		SPE_HipFire_Only = 0;
 		class EventHandlers : EventHandlers
 		{
@@ -101,7 +101,7 @@ class CfgWeapons
 	};
 	class uk3cb_mg42_base : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldMG42";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -114,7 +114,7 @@ class CfgWeapons
 	};
 	class uk3cb_uk59 : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldM240";
+		SPE_deployment_animation = "RHS_GestureHoldM240";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -139,6 +139,7 @@ class CfgWeapons
 	};
 	class rhs_weap_m84 : rifle_long_base_f
 	{
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -151,7 +152,7 @@ class CfgWeapons
 	};
 	class lmg_s77_base_lxws : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldM240";
+		SPE_deployment_animation = "RHS_GestureHoldM240";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -174,7 +175,7 @@ class CfgWeapons
 	
 	class gm6_base_f : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -205,7 +206,7 @@ class CfgWeapons
 	};
 	class gm_mg3_base : gm_machinegun_base
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldMG42";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -221,7 +222,7 @@ class CfgWeapons
 	{};
 	class gm_pkm_base : gm_pk_base
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldPK";
+		SPE_deployment_animation = "RHS_GestureHoldPK";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -239,7 +240,7 @@ class CfgWeapons
 	};
 	class uk3cb_baf_l135a1 : srifle_gm6_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldM107";
+		SPE_deployment_animation = "RHS_GestureHoldM107";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -253,7 +254,7 @@ class CfgWeapons
 	
 	class UK3CB_M60_base : rifle_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldM240";
+		SPE_deployment_animation = "RHS_GestureHoldM240";
 		SPE_HipFire_Only = 1;
 		class EventHandlers : EventHandlers
 		{
@@ -292,99 +293,99 @@ class CfgWeapons
 	class uk3cb_baf_l85a2;
 	class uk3cb_baf_l86a2 : uk3cb_baf_l85a2
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldMG42";
 	};
 	
 	class rhs_weap_XM2010_Base_F : Rifle_Base_F
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class lmg_zafir_f : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class uk3cb_g3_base;
 	class uk3cb_psg1a1 : uk3cb_g3_base
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldMG42";
 	};
 	
 	class gm_g3_base;
 	class gm_psg1_base : gm_g3_base
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldMG42";
 	};
 	class gm_msg90_base : gm_psg1_base
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class uk3cb_baf_l110_base : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class rhs_weap_saw_base : rifle_base_f
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class rhs_weap_lmg_minimi_railed;
 	class rhs_weap_m249_pip : rhs_weap_lmg_minimi_railed
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldM240";
+		SPE_deployment_animation = "RHS_GestureHoldM240";
 	};
 	
 	class rhs_weap_orsis_base_f;
 	class rhs_weap_t5000 : rhs_weap_orsis_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldMG42";
 	};
 	
 	class gm_g8_base;
 	class gm_mg8_base : gm_g8_base
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class dmr_07_base_f : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class arifle_ak12_base_f;
 	class arifle_rpk12_base_f : arifle_ak12_base_f
 	{
-		rhs_deployedHandAnim = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldMG42";
 	};
 	
 	class gm_ak47_base;
 	class gm_rpk_base : gm_ak47_base
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class gm_ak74_base;
 	class gm_rpk74_base : gm_ak74_base
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class rhs_weap_rpk_base : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class uk3cb_g36_base;
 	class uk3cb_g36_lsw : uk3cb_g36_base
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	
 	class lmg_03_base_f : rifle_long_base_f
 	{
-		rhs_deployedHandAnim = "gestureAltHoldMG42v2";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 };
