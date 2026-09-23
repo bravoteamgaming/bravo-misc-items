d78bf402ef82b1ac9156c8fa7b53ea42bcc07c6a
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-09-23 16:31:43 +0100

    path fix

--- bravo_gear_misc/cfgWeapons/efvests.hpp
+++ bravo_gear_misc/cfgWeapons/efvests.hpp
@@ -4,42 +4,42 @@
 	class bravo_ef_aav_diver_alt_black : ef_v_aav_diver_black
 	{
 		displayName = "Amphibious Assault Vest (Black/Diver/Alt)";
-		model = "ef/ef_marines/ef_v_aav_diver_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_diver_alt.p3d";
 	};
 	
 	class ef_v_aav_diver_noreb_black;
 	class bravo_ef_aav_diver_noreb_alt_black : ef_v_aav_diver_noreb_black
 	{
 		displayName = "Amphibious Assault Vest (Black/Diver/No Rebreather/Alt)";
-		model = "ef/ef_marines/ef_v_aav_diver_noreb_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_diver_noreb_alt.p3d";
 	};
 	
 	class ef_v_aav_rifleman_black;
 	class bravo_ef_aav_rifleman_alt_black : ef_v_aav_rifleman_black
 	{
 		displayName = "Amphibious Assault Vest (Black/Rifleman/Alt)";
-		model = "ef/ef_marines/ef_v_aav_rifleman_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_rifleman_alt.p3d";
 	};
 	
 	class ef_v_aav_sailor_black;
 	class bravo_ef_aav_sailor_alt_black : ef_v_aav_sailor_black
 	{
 		displayName = "Amphibious Assault Vest (Black/Sailor/Alt)";
-		model = "ef/ef_marines/ef_v_aav_sailor_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_sailor_alt.p3d";
 	};
 	
 	class ef_v_aav_scout_black;
 	class bravo_ef_aav_scout_alt_black : ef_v_aav_scout_black
 	{
 		displayName = "Amphibious Assault Vest (Black/Scout/Alt)";
-		model = "ef/ef_marines/ef_v_aav_scout_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_scout_alt.p3d";
 	};
 	
 	class ef_v_aav_tl_black;
 	class bravo_ef_aav_tl_alt_black : ef_v_aav_tl_black
 	{
 		displayName = "Amphibious Assault Vest (Black/Team Leader/Alt)";
-		model = "ef/ef_marines/ef_v_aav_tl_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_tl_alt.p3d";
 	};
 	
 	// =========== AAV COYOTE
@@ -48,42 +48,42 @@
 	class bravo_ef_aav_diver_alt_coy : ef_v_aav_diver_coy
 	{
 		displayName = "Amphibious Assault Vest (Coyote Brown/Diver/Alt)";
-		model = "ef/ef_marines/ef_v_aav_diver_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_diver_alt.p3d";
 	};
 	
 	class ef_v_aav_diver_noreb_coy;
 	class bravo_ef_aav_diver_noreb_alt_coy : ef_v_aav_diver_noreb_coy
 	{
 		displayName = "Amphibious Assault Vest (Coyote Brown/Diver/No Rebreather/Alt)";
-		model = "ef/ef_marines/ef_v_aav_diver_noreb_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_diver_noreb_alt.p3d";
 	};
 	
 	class ef_v_aav_rifleman_coy;
 	class bravo_ef_aav_rifleman_alt_coy : ef_v_aav_rifleman_coy
 	{
 		displayName = "Amphibious Assault Vest (Coyote Brown/Rifleman/Alt)";
-		model = "ef/ef_marines/ef_v_aav_rifleman_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_rifleman_alt.p3d";
 	};
 	
 	class ef_v_aav_sailor_coy;
 	class bravo_ef_aav_sailor_alt_coy : ef_v_aav_sailor_coy
 	{
 		displayName = "Amphibious Assault Vest (Coyote Brown/Sailor/Alt)";
-		model = "ef/ef_marines/ef_v_aav_sailor_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_sailor_alt.p3d";
 	};
 	
 	class ef_v_aav_scout_coy;
 	class bravo_ef_aav_scout_alt_coy : ef_v_aav_scout_coy
 	{
 		displayName = "Amphibious Assault Vest (Coyote Brown/Scout/Alt)";
-		model = "ef/ef_marines/ef_v_aav_scout_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_scout_alt.p3d";
 	};
 	
 	class ef_v_aav_tl_coy;
 	class bravo_ef_aav_tl_alt_coy : ef_v_aav_tl_coy
 	{
 		displayName = "Amphibious Assault Vest (Coyote Brown/Team Leader/Alt)";
-		model = "ef/ef_marines/ef_v_aav_tl_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_tl_alt.p3d";
 	};
 	
 	// ============= AAV OLIVE
@@ -92,42 +92,42 @@
 	class bravo_ef_aav_diver_alt_olive : ef_v_aav_diver_olive
 	{
 		displayName = "Amphibious Assault Vest (Olive/Diver/Alt)";
-		model = "ef/ef_marines/ef_v_aav_diver_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_diver_alt.p3d";
 	};
 	
 	class ef_v_aav_diver_noreb_olive;
 	class bravo_ef_aav_diver_noreb_alt_olive : ef_v_aav_diver_noreb_olive
 	{
 		displayName = "Amphibious Assault Vest (Olive/Diver/No Rebreather/Alt)";
-		model = "ef/ef_marines/ef_v_aav_diver_noreb_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_diver_noreb_alt.p3d";
 	};
 	
 	class ef_v_aav_rifleman_olive;
 	class bravo_ef_aav_rifleman_alt_olive : ef_v_aav_rifleman_olive
 	{
 		displayName = "Amphibious Assault Vest (Olive/Rifleman/Alt)";
-		model = "ef/ef_marines/ef_v_aav_rifleman_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_rifleman_alt.p3d";
 	};
 	
 	class ef_v_aav_sailor_olive;
 	class bravo_ef_aav_sailor_alt_olive : ef_v_aav_sailor_olive
 	{
 		displayName = "Amphibious Assault Vest (Olive/Sailor/Alt)";
-		model = "ef/ef_marines/ef_v_aav_sailor_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_sailor_alt.p3d";
 	};
 	
 	class ef_v_aav_scout_olive;
 	class bravo_ef_aav_scout_alt_olive : ef_v_aav_scout_olive
 	{
 		displayName = "Amphibious Assault Vest (Olive/Scout/Alt)";
-		model = "ef/ef_marines/ef_v_aav_scout_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_scout_alt.p3d";
 	};
 	
 	class ef_v_aav_tl_olive;
 	class bravo_ef_aav_tl_alt_olive : ef_v_aav_tl_olive
 	{
 		displayName = "Amphibious Assault Vest (Olive/Team Leader/Alt)";
-		model = "ef/ef_marines/ef_v_aav_tl_alt.p3d";
+		model = "\ef\ef_marines/ef_v_aav_tl_alt.p3d";
 	};
 	
 	// ================== CCR BLACK
@@ -136,21 +136,21 @@
 	class bravo_ef_ccr_rifleman_alt_black : ef_v_ccr_rifleman_black
 	{
 		displayName = "Commando Chest Rig (Black/Rifleman/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_rifleman_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_rifleman_alt.p3d";
 	};
 	
 	class ef_v_ccr_scout_black;
 	class bravo_ef_ccr_scout_alt_black : ef_v_ccr_scout_black
 	{
 		displayName = "Commando Chest Rig (Black/Scout/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_scout_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_scout_alt.p3d";
 	};
 	
 	class ef_v_ccr_tl_black;
 	class bravo_ef_ccr_tl_alt_black : ef_v_ccr_tl_black
 	{
 		displayName = "Commando Chest Rig (Black/Team Leader/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_tl_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_tl_alt.p3d";
 	};
 	
 	// ======== CCR COYOTE
@@ -159,21 +159,21 @@
 	class bravo_ef_ccr_rifleman_alt_coy : ef_v_ccr_rifleman_coy
 	{
 		displayName = "Commando Chest Rig (Coyote Brown/Rifleman/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_rifleman_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_rifleman_alt.p3d";
 	};
 	
 	class ef_v_ccr_scout_coy;
 	class bravo_ef_ccr_scout_alt_coy : ef_v_ccr_scout_coy
 	{
 		displayName = "Commando Chest Rig (Coyote Brown/Scout/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_scout_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_scout_alt.p3d";
 	};
 	
 	class ef_v_ccr_tl_coy;
 	class bravo_ef_ccr_tl_alt_coy : ef_v_ccr_tl_coy
 	{
 		displayName = "Commando Chest Rig (Coyote Brown/Team Leader/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_tl_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_tl_alt.p3d";
 	};
 	
 	// ============= CCR OLIVE
@@ -182,19 +182,19 @@
 	class bravo_ef_ccr_rifleman_alt_olive : ef_v_ccr_rifleman_olive
 	{
 		displayName = "Commando Chest Rig (Olive/Rifleman/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_rifleman_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_rifleman_alt.p3d";
 	};
 	
 	class ef_v_ccr_scout_olive;
 	class bravo_ef_ccr_scout_alt_olive : ef_v_ccr_scout_olive
 	{
 		displayName = "Commando Chest Rig (Olive/Scout/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_scout_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_scout_alt.p3d";
 	};
 	
 	class ef_v_ccr_tl_olive;
 	class bravo_ef_ccr_tl_alt_olive : ef_v_ccr_tl_olive
 	{
 		displayName = "Commando Chest Rig (Olive/Team Leader/Alt)";
-		model = "ef/ef_marines/ef_v_ccr_tl_alt.p3d";
+		model = "\ef\ef_marines/ef_v_ccr_tl_alt.p3d";
 	};
