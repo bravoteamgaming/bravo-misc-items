6b86b7a58fefc9eb5481740d2c91974a650cadc8
NikkoJT <35894105+NikkoJT@users.noreply.github.com>
2026-09-24 02:27:10 +0100

    anim tweak

--- bravo_spehipfire/cfgWeapons.hpp
+++ bravo_spehipfire/cfgWeapons.hpp
@@ -388,6 +388,7 @@ class CfgWeapons
 	class vn_m60 : vn_lmg
 	{
 		SPE_HipfireOnly = 1;
+		SPE_deployment_animation = "RHS_GestureHoldM240";
 	};
 	class vn_mg42 : vn_lmg
 	{
@@ -397,14 +398,14 @@ class CfgWeapons
 	class vn_pk : vn_lmg
 	{
 		SPE_HipfireOnly = 1;
-		SPE_deployment_animation = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "RHS_GestureHoldPK";
 	};
 	class vn_l4 : vn_lmg
 	{
-		SPE_deployment_animation = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 	class vnx_fm2429 : vn_lmg
 	{
-		SPE_deployment_animation = "RHS_GestureHoldMG42";
+		SPE_deployment_animation = "gestureAltHoldMG42v2";
 	};
 };
